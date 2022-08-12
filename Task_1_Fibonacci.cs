a1ee20875f686e2a974fdd6c468083b31306c465
marcel-meursault <marcelmeursault@icloud.com>
2022-08-12 23:41:13 +0300

    Click2money_Naumov

---
+++ Task_1_Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                var firstNumber = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var secondNumber = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(firstNumber + secondNumber);
+            }
+            foreach (var item in fibonacciNumbers)
+                Console.Write(item + " ");
+        }
+    }
+}
